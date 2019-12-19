de28cc9aaacef2d4f176508c06a36a3d60146d1a
Steve Gordon <steve.gordon@madgex.com>
2019-12-19 09:27:15 +0000

    Adding an example for unit testing against this library

---
+++ samples/3.1/MvcSample/ServiceWhichUsesCorrelationContext.cs
@@ -0,0 +1,21 @@
+﻿using CorrelationId;
+
+namespace MvcSample
+{
+    public class ServiceWhichUsesCorrelationContext
+    {
+        private readonly ICorrelationContextAccessor _correlationContextAccessor;
+
+        public ServiceWhichUsesCorrelationContext(ICorrelationContextAccessor correlationContextAccessor)
+        {
+            _correlationContextAccessor = correlationContextAccessor;
+        }
+
+        public string DoStuff()
+        {
+            var correlationId = _correlationContextAccessor.CorrelationContext.CorrelationId;
+
+            return $"Formatted correlation ID:{correlationId}";
+        }
+    }
+}
