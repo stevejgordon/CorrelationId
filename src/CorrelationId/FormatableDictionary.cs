ae67222e69170e1703209aee2c4d882380fce68b
billshu <bill.shu@gravitysupplychain.com>
2021-02-11 14:38:27 +0800

    Make sure the scope value of correlation id can be printed out in console logger

---
+++ src/CorrelationId/FormatableDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CorrelationId
+{
+    /// <summary>
+    /// Override ToString of Dictionary so that when printed using Console logger
+    /// correlation id can be printed out instead of just print the type name
+    /// </summary>
+    public class FormatableDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        /// <summary>
+        /// The joined key value pairs of the dictionary. If empty, will simply return []
+        /// </summary>
+        public override string ToString()
+        {
+            var keyValuePairs = this.Select(x => $"{x.Key}:{x.Value}").ToArray();
+            if (keyValuePairs.Any() is false)
+            {
+                return "[]";
+            }
+            var joinedKeyValuePairs = string.Join(",", keyValuePairs);
+            return joinedKeyValuePairs;
+        }
+    }
+}
